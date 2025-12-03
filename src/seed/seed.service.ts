3d412caa0a92f9191c70d83bdafe4107f860b0d2
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-12-03 12:13:48 -0300

    feat(seed): implementar método executeSeed para obtener datos de Pokémon

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  constructor() {}
+  
+  private readonly axios: AxiosInstance = axios;
 
-  executeSeed() {
-    return 'This action seeds the database';
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+
+    // Extraemos los datos que necesitamos de la respuesta de la API
+    data.results.forEach(({ name, url }) => {
+      
+      // Con split podemos dividir un string en varios segmentos (aca lo hacemos con el caracter '/')
+      const segments = url.split('/');
+      // El no del pokemon siempre esta en la penultima posicion
+      // tiene que ser un numero, por eso usamos el operador '+' para convertir el string a number
+      const no: number = +segments[ segments.length - 2];
+      console.log({ name, no })
+    })
+    
+    return data.results;
   }
 }
