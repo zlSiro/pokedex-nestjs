85d403c3890dcd9b4321b6d25039f1941050017f
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-12-04 10:32:15 -0300

    feat(seed): agregar módulo de Pokémon al módulo de Seed

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { PokemonModule } from 'src/pokemon/pokemon.module';
 
 @Module({
+  imports: [PokemonModule],
   controllers: [SeedController],
   providers: [SeedService],
   exports: [SeedService],
