227c1917882438db07632bcb0cd23ae31add7f24
JuanPablo Valdebenito <jpablo.valdebenitom@gmail.com>
2025-12-04 10:32:27 -0300

    feat(seed): optimizar método executeSeed para insertar pokemones

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,25 +1,77 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 
 @Injectable()
 export class SeedService {
   
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    // Inyectamos el modelo de Pokémon para poder crear registros en la base de datos
+    // Para eso tenemos que importar el módulo de Pokémon en el SeedModule
+    @InjectModel( Pokemon.name ) // Inyecta el MODELO de Mongoose, no el PokemonService
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   async executeSeed() {
+
+    /**
+     *  HAY VARIAS FORMAS DE HACER ESTO, ESTA ES UNA DE ELLAS.
+     
+
+    // Cada vez que ejecutamos este metodo, limpiamos la colección de pokemones
+    await this.pokemonModel.deleteMany({}); // DELETE * FROM pokemons en SQL
+
+    // Hacemos la petición HTTP a la PokeAPI
     const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+
+    // Creamos un arreglo de promesas para crear los pokemones en la base de datos
+    const insertPromisesArray: Promise<any>[] = [];
 
     // Extraemos los datos que necesitamos de la respuesta de la API
     data.results.forEach(({ name, url }) => {
       
       // Con split podemos dividir un string en varios segmentos (aca lo hacemos con el caracter '/')
       const segments = url.split('/');
+
       // El no del pokemon siempre esta en la penultima posicion
       // tiene que ser un numero, por eso usamos el operador '+' para convertir el string a number
       const no: number = +segments[ segments.length - 2];
-      console.log({ name, no })
-    })    
-    return data.results;
+
+      // También usas this.pokemonModel.create() - método de Mongoose
+      // const pokemon = await this.pokemonModel.create({ name, no });
+
+      insertPromisesArray.push(
+        this.pokemonModel.create({ name, no })
+      );
+    })
+
+    await Promise.all(insertPromisesArray);
+
+    */
+
+    // Forma más eficiente de hacer el seed usando insertMany
+    
+    await this.pokemonModel.deleteMany({}); // DELETE * FROM pokemons en SQL
+    
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
+
+    const pokemonToInsert: { name: string, no: number }[] = [];
+    
+    data.results.forEach( ({ name, url }) => {
+      const segments = url.split('/');
+      const no: number = +segments[ segments.length - 2];
+
+      pokemonToInsert.push({ name, no });
+    });
+    // Insertamos todos los pokemones de una sola vez en la base de datos
+    await this.pokemonModel.insertMany( pokemonToInsert );
+
+    return 'Seed executed successfully';
   }
 }
